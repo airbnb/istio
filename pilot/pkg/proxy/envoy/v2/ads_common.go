cbe736ab411aff7b0dbfa0a64b64af1da958320d
Weibo He <weibo.he@airbnb.com>
2020-05-02 09:13:04 -0700

    Revert "Update RDS on DestinationRule change (#23448)"
    This reverts commit 974fb4d1257754187f2c84328cb759ef483a1d7b.

--- pilot/pkg/proxy/envoy/v2/ads_common.go
+++ pilot/pkg/proxy/envoy/v2/ads_common.go
@@ -125,7 +125,6 @@ func PushTypeFor(proxy *model.Proxy, pushEv *XdsEvent) map[XdsType]bool {
 			case collections.IstioNetworkingV1Alpha3Destinationrules.Resource().GroupVersionKind():
 				out[CDS] = true
 				out[EDS] = true
-				out[RDS] = true
 			case collections.IstioNetworkingV1Alpha3Envoyfilters.Resource().GroupVersionKind():
 				out[CDS] = true
 				out[EDS] = true
