4e7d9abcea3bdc6976925407765936617a6abc8e
Weibo He <weibo.he@airbnb.com>
2020-05-02 09:13:04 -0700

    Revert "support define `IsServer`  when generating cert (#23106)"
    This reverts commit 36663d57bcacf92e6fc1a0125f6d88f99a748307.

--- security/tools/generate_cert/main.go
+++ security/tools/generate_cert/main.go
@@ -55,8 +55,6 @@ var (
 	outPriv        = flag.String("out-priv", "priv.pem", "Output private key file.")
 	keySize        = flag.Int("key-size", 2048, "Size of the generated private key")
 	mode           = flag.String("mode", selfSignedMode, "Supported mode: self-signed, signer, citadel")
-	//Enable this flag if istio mTLS is enabled and the service is running as server side
-	isServer = flag.Bool("server", false, "Whether this certificate is for a server.")
 )
 
 func checkCmdLine() {
@@ -147,7 +145,6 @@ func main() {
 		IsSelfSigned: *mode == selfSignedMode,
 		IsClient:     *isClient,
 		RSAKeySize:   *keySize,
-		IsServer:     *isServer,
 	}
 	certPem, privPem, err := util.GenCertKeyFromOptions(opts)
 
