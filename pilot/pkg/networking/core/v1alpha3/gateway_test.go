3dcf4000c6171f335b4fbefc71ec6594b6bc8e99
Weibo He <weibo.he@airbnb.com>
2020-05-02 09:13:04 -0700

    Revert "Added ability to configure Gateway Topology (#23292)"
    This reverts commit f3d4367ebad328670b2d98eb1eba31632eac7536.

--- pilot/pkg/networking/core/v1alpha3/gateway_test.go
+++ pilot/pkg/networking/core/v1alpha3/gateway_test.go
@@ -25,7 +25,6 @@ import (
 
 	networking "istio.io/api/networking/v1alpha3"
 
-	meshconfig "istio.io/api/mesh/v1alpha1"
 	"istio.io/istio/pilot/pkg/features"
 	pilot_model "istio.io/istio/pilot/pkg/model"
 	"istio.io/istio/pilot/pkg/networking/core/v1alpha3/fakes"
@@ -552,12 +551,11 @@ func TestBuildGatewayListenerTlsContext(t *testing.T) {
 
 func TestCreateGatewayHTTPFilterChainOpts(t *testing.T) {
 	testCases := []struct {
-		name        string
-		node        *pilot_model.Proxy
-		server      *networking.Server
-		routeName   string
-		proxyConfig *meshconfig.ProxyConfig
-		result      *filterChainOpts
+		name      string
+		node      *pilot_model.Proxy
+		server    *networking.Server
+		routeName string
+		result    *filterChainOpts
 	}{
 		{
 			name: "HTTP1.0 mode enabled",
@@ -567,8 +565,7 @@ func TestCreateGatewayHTTPFilterChainOpts(t *testing.T) {
 			server: &networking.Server{
 				Port: &networking.Port{},
 			},
-			routeName:   "some-route",
-			proxyConfig: nil,
+			routeName: "some-route",
 			result: &filterChainOpts{
 				sniHosts:   nil,
 				tlsContext: nil,
@@ -576,7 +573,6 @@ func TestCreateGatewayHTTPFilterChainOpts(t *testing.T) {
 					rds:              "some-route",
 					useRemoteAddress: true,
 					connectionManager: &http_conn.HttpConnectionManager{
-						XffNumTrustedHops:        0,
 						ForwardClientCertDetails: http_conn.HttpConnectionManager_SANITIZE_SET,
 						SetCurrentClientCertDetails: &http_conn.HttpConnectionManager_SetCurrentClientCertDetails{
 							Subject: proto.BoolTrue,
@@ -604,8 +600,7 @@ func TestCreateGatewayHTTPFilterChainOpts(t *testing.T) {
 					Mode: networking.ServerTLSSettings_ISTIO_MUTUAL,
 				},
 			},
-			routeName:   "some-route",
-			proxyConfig: nil,
+			routeName: "some-route",
 			result: &filterChainOpts{
 				sniHosts: []string{"example.org"},
 				tlsContext: &auth.DownstreamTlsContext{
@@ -641,7 +636,6 @@ func TestCreateGatewayHTTPFilterChainOpts(t *testing.T) {
 					rds:              "some-route",
 					useRemoteAddress: true,
 					connectionManager: &http_conn.HttpConnectionManager{
-						XffNumTrustedHops:        0,
 						ForwardClientCertDetails: http_conn.HttpConnectionManager_SANITIZE_SET,
 						SetCurrentClientCertDetails: &http_conn.HttpConnectionManager_SetCurrentClientCertDetails{
 							Subject: proto.BoolTrue,
@@ -667,8 +661,7 @@ func TestCreateGatewayHTTPFilterChainOpts(t *testing.T) {
 					Mode: networking.ServerTLSSettings_ISTIO_MUTUAL,
 				},
 			},
-			routeName:   "some-route",
-			proxyConfig: nil,
+			routeName: "some-route",
 			result: &filterChainOpts{
 				sniHosts: []string{"example.org", "test.org"},
 				tlsContext: &auth.DownstreamTlsContext{
@@ -704,7 +697,6 @@ func TestCreateGatewayHTTPFilterChainOpts(t *testing.T) {
 					rds:              "some-route",
 					useRemoteAddress: true,
 					connectionManager: &http_conn.HttpConnectionManager{
-						XffNumTrustedHops:        0,
 						ForwardClientCertDetails: http_conn.HttpConnectionManager_SANITIZE_SET,
 						SetCurrentClientCertDetails: &http_conn.HttpConnectionManager_SetCurrentClientCertDetails{
 							Subject: proto.BoolTrue,
@@ -730,8 +722,7 @@ func TestCreateGatewayHTTPFilterChainOpts(t *testing.T) {
 					Mode: networking.ServerTLSSettings_ISTIO_MUTUAL,
 				},
 			},
-			routeName:   "some-route",
-			proxyConfig: nil,
+			routeName: "some-route",
 			result: &filterChainOpts{
 				sniHosts: []string{"*.example.org", "example.org"},
 				tlsContext: &auth.DownstreamTlsContext{
@@ -767,110 +758,7 @@ func TestCreateGatewayHTTPFilterChainOpts(t *testing.T) {
 					rds:              "some-route",
 					useRemoteAddress: true,
 					connectionManager: &http_conn.HttpConnectionManager{
-						XffNumTrustedHops:        0,
 						ForwardClientCertDetails: http_conn.HttpConnectionManager_SANITIZE_SET,
-						SetCurrentClientCertDetails: &http_conn.HttpConnectionManager_SetCurrentClientCertDetails{
-							Subject: proto.BoolTrue,
-							Cert:    true,
-							Uri:     true,
-							Dns:     true,
-						},
-						ServerName:          EnvoyServerName,
-						HttpProtocolOptions: &core.Http1ProtocolOptions{},
-					},
-				},
-			},
-		},
-		{
-			name: "Topology HTTP Protocol",
-			node: &pilot_model.Proxy{Metadata: &pilot_model.NodeMetadata{}},
-			server: &networking.Server{
-				Port: &networking.Port{},
-			},
-			routeName: "some-route",
-			proxyConfig: &meshconfig.ProxyConfig{
-				GatewayTopology: &meshconfig.Topology{
-					NumTrustedProxies:        2,
-					ForwardClientCertDetails: meshconfig.Topology_APPEND_FORWARD,
-				},
-			},
-			result: &filterChainOpts{
-				sniHosts:   nil,
-				tlsContext: nil,
-				httpOpts: &httpListenerOpts{
-					rds:              "some-route",
-					useRemoteAddress: true,
-					connectionManager: &http_conn.HttpConnectionManager{
-						XffNumTrustedHops:        2,
-						ForwardClientCertDetails: http_conn.HttpConnectionManager_APPEND_FORWARD,
-						SetCurrentClientCertDetails: &http_conn.HttpConnectionManager_SetCurrentClientCertDetails{
-							Subject: proto.BoolTrue,
-							Cert:    true,
-							Uri:     true,
-							Dns:     true,
-						},
-						ServerName:          EnvoyServerName,
-						HttpProtocolOptions: &core.Http1ProtocolOptions{},
-					},
-				},
-			},
-		},
-		{
-			name: "Topology HTTPS Protocol",
-			node: &pilot_model.Proxy{Metadata: &pilot_model.NodeMetadata{}},
-			server: &networking.Server{
-				Port: &networking.Port{
-					Protocol: "HTTPS",
-				},
-				Hosts: []string{"example.org"},
-				Tls: &networking.ServerTLSSettings{
-					Mode: networking.ServerTLSSettings_ISTIO_MUTUAL,
-				},
-			},
-			routeName: "some-route",
-			proxyConfig: &meshconfig.ProxyConfig{
-				GatewayTopology: &meshconfig.Topology{
-					NumTrustedProxies:        3,
-					ForwardClientCertDetails: meshconfig.Topology_FORWARD_ONLY,
-				},
-			},
-			result: &filterChainOpts{
-				sniHosts: []string{"example.org"},
-				tlsContext: &auth.DownstreamTlsContext{
-					RequireClientCertificate: proto.BoolTrue,
-					CommonTlsContext: &auth.CommonTlsContext{
-						TlsCertificates: []*auth.TlsCertificate{
-							{
-								CertificateChain: &core.DataSource{
-									Specifier: &core.DataSource_Filename{
-										Filename: "/etc/certs/cert-chain.pem",
-									},
-								},
-								PrivateKey: &core.DataSource{
-									Specifier: &core.DataSource_Filename{
-										Filename: "/etc/certs/key.pem",
-									},
-								},
-							},
-						},
-						ValidationContextType: &auth.CommonTlsContext_ValidationContext{
-							ValidationContext: &auth.CertificateValidationContext{
-								TrustedCa: &core.DataSource{
-									Specifier: &core.DataSource_Filename{
-										Filename: "/etc/certs/root-cert.pem",
-									},
-								},
-							},
-						},
-						AlpnProtocols: []string{"h2", "http/1.1"},
-					},
-				},
-				httpOpts: &httpListenerOpts{
-					rds:              "some-route",
-					useRemoteAddress: true,
-					connectionManager: &http_conn.HttpConnectionManager{
-						XffNumTrustedHops:        3,
-						ForwardClientCertDetails: http_conn.HttpConnectionManager_FORWARD_ONLY,
 						SetCurrentClientCertDetails: &http_conn.HttpConnectionManager_SetCurrentClientCertDetails{
 							Subject: proto.BoolTrue,
 							Cert:    true,
@@ -887,9 +775,9 @@ func TestCreateGatewayHTTPFilterChainOpts(t *testing.T) {
 
 	for _, tc := range testCases {
 		cgi := NewConfigGenerator([]plugin.Plugin{})
-		ret := cgi.createGatewayHTTPFilterChainOpts(tc.node, tc.server, tc.routeName, "", tc.proxyConfig)
+		ret := cgi.createGatewayHTTPFilterChainOpts(tc.node, tc.server, tc.routeName, "")
 		if !reflect.DeepEqual(tc.result, ret) {
-			t.Errorf("test case %s: expecting %+v but got %+v", tc.name, tc.result.httpOpts.connectionManager, ret.httpOpts.connectionManager)
+			t.Errorf("test case %s: expecting %v but got %v", tc.name, tc.result, ret)
 		}
 	}
 }
