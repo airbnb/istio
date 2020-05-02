68a77a63f19fe79609121244441f9df7da9f4d06
Weibo He <weibo.he@airbnb.com>
2020-05-02 09:13:04 -0700

    Revert "send more requests (#23007)"
    This reverts commit 55197c44b8e8cbe822d27b2a99969280f7bdf49e.

--- security/pkg/nodeagent/test/cert_rotation/rotation_test.go
+++ security/pkg/nodeagent/test/cert_rotation/rotation_test.go
@@ -24,6 +24,7 @@ import (
 )
 
 func TestCertRotation(t *testing.T) {
+	t.Skip("https://github.com/istio/istio/issues/22729")
 	rotateInterval := 1 * time.Second
 	sdsTest.RotateCert(rotateInterval)
 	setup := sdsTest.SetupTest(t, env.SDSCertRotation)
@@ -41,7 +42,7 @@ func TestCertRotation(t *testing.T) {
 			t.Errorf("Unexpected status code: %d", code)
 		}
 		numReq++
-		if time.Since(start) > 4*rotateInterval {
+		if time.Since(start) > 2*rotateInterval {
 			break
 		}
 		time.Sleep(100 * time.Millisecond)
