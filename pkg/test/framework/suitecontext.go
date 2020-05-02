32f5e7d68e04e9f610a6bca00da5a6767104a375
Weibo He <weibo.he@airbnb.com>
2020-05-02 09:13:04 -0700

    Revert "Allow Integ Tests to declare feature coverage (#22760)"
    This reverts commit bfc629e6f7bbfbd35cb69f51314c8c8e55ea1cd7.

--- pkg/test/framework/suitecontext.go
+++ pkg/test/framework/suitecontext.go
@@ -23,8 +23,6 @@ import (
 	"strings"
 	"sync"
 
-	"istio.io/istio/pkg/test/framework/features"
-
 	"istio.io/istio/pkg/test/framework/label"
 	"istio.io/istio/pkg/test/framework/resource"
 	"istio.io/istio/pkg/test/scopes"
@@ -52,8 +50,7 @@ type suiteContext struct {
 	contextMu    sync.Mutex
 	contextNames map[string]struct{}
 
-	suiteLabels  label.Set
-	testOutcomes []TestOutcome
+	suiteLabels label.Set
 }
 
 func newSuiteContext(s *resource.Settings, envFn resource.EnvironmentFactory, labels label.Set) (*suiteContext, error) {
@@ -159,44 +156,8 @@ func (s *suiteContext) CreateTmpDirectory(prefix string) (string, error) {
 		scopes.Framework.Errorf("Error creating temp dir: runID='%s', prefix='%s', workDir='%v', err='%v'",
 			s.settings.RunID, prefix, s.workDir, err)
 	} else {
-		scopes.Framework.Debugf("Created a temp dir: runID='%s', Name='%s'", s.settings.RunID, dir)
+		scopes.Framework.Debugf("Created a temp dir: runID='%s', name='%s'", s.settings.RunID, dir)
 	}
 
 	return dir, err
-}
-
-type Outcome string
-
-const (
-	Passed         Outcome = "Passed"
-	Failed         Outcome = "Failed"
-	Skipped        Outcome = "Skipped"
-	NotImplemented Outcome = "NotImplemented"
-)
-
-type TestOutcome struct {
-	Name          string
-	Type          string
-	Outcome       Outcome
-	FeatureLabels []features.Feature
-}
-
-func (s *suiteContext) registerOutcome(test *Test) {
-	o := Passed
-	if test.notImplemented {
-		o = NotImplemented
-	} else if test.goTest.Failed() {
-		o = Failed
-	} else if test.goTest.Skipped() {
-		o = Skipped
-	}
-	newOutcome := TestOutcome{
-		Name:          test.goTest.Name(),
-		Type:          "integration",
-		Outcome:       o,
-		FeatureLabels: test.featureLabels,
-	}
-	s.contextMu.Lock()
-	defer s.contextMu.Unlock()
-	s.testOutcomes = append(s.testOutcomes, newOutcome)
 }
