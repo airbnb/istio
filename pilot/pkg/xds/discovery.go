06b7902c15946a4532bb6e4264fce8311f7e515b
edie_yang <edie.yang@airbnb.com>
2023-07-28 12:52:42 -0700

    istio: fix debounce metrics
    cherry-pick https://gerrit.musta.ch/c/public/istio/+/4583.
    
    Change-Id: I1ac8f834f0ab232e9aa81295724bd0e003c2a6f6
    Reviewed-on: https://gerrit.musta.ch/c/public/istio/+/5527
    Reviewed-by: Douglas Jordan <douglas.jordan@airbnb.com>

--- pilot/pkg/xds/discovery.go
+++ pilot/pkg/xds/discovery.go
@@ -365,7 +365,6 @@ func debounce(ch chan *model.PushRequest, stopCh <-chan struct{}, opts debounceO
 	push := func(req *model.PushRequest, debouncedEvents int, startDebounce time.Time) {
 		pushFn(req)
 		updateSent.Add(int64(debouncedEvents))
-		debounceTime.Record(time.Since(startDebounce).Seconds())
 		freeCh <- struct{}{}
 	}
 
@@ -385,6 +384,7 @@ func debounce(ch chan *model.PushRequest, stopCh <-chan struct{}, opts debounceO
 						pushCounter, debouncedEvents, configsUpdated(req),
 						quietTime, eventDelay, req.Full)
 				}
+				debounceTime.Record(eventDelay.Seconds())
 				free = false
 				go push(req, debouncedEvents, startDebounce)
 				req = nil
